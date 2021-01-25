304e2801bc18f37be2fecf9f84bee5a74f715e3e
SydNS <sydneynsiimbe@gmail.com>
2021-01-25 05:54:10 -0800

    added viewbinding

--- app/src/main/java/com/example/tascs/LoginActivity.kt
+++ app/src/main/java/com/example/tascs/LoginActivity.kt
@@ -7,6 +7,9 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
