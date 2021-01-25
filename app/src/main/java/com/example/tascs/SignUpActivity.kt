b53cc6ed9509282a22c059fb78d5220449b9fe81
SydNS <sydneynsiimbe@gmail.com>
2021-01-25 01:03:15 -0800

    removed old hooks for the erased changes from yesterday

--- app/src/main/java/com/example/tascs/SignUpActivity.kt
+++ app/src/main/java/com/example/tascs/SignUpActivity.kt
@@ -1,12 +1,29 @@
 package com.example.tascs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+//        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+
+//        val emailaddress=emailsignup.text
+//        val passtxt=passsignup.text
+//        btnsignup.setOnClickListener {
+//            startActivity(Intent(this ,LoginActivity::class.java)
+//                .putExtra("Useremail",emailaddress)
+//                )
+//            finish()
+//        }
+
+
+
 
 
 
