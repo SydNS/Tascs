35b13cdc318875e9b57940bcc0cfd982e6613510
SydNS <sydneynsiimbe@gmail.com>
2021-01-25 01:02:19 -0800

    added some action to the activity to test whether everything was in line

--- app/src/main/java/com/example/tascs/LoginActivity.kt
+++ app/src/main/java/com/example/tascs/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tascs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        signuplink.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+                finish()
+//            Toast.makeText(this,"deands",Toast.LENGTH_LONG)
+//                .show()
+        }
     }
 }
