073a07b25bfd6ddd84a5b997f33edf9ddc4f0ddc
SydNS <sydneynsiimbe@gmail.com>
2021-01-25 22:50:20 -0800

    created a view and assigned it to root in the binding(which is a layout holding other views ) and then use this view to access its children (inside views)

--- app/src/main/java/com/example/tascs/LoginActivity.kt
+++ app/src/main/java/com/example/tascs/LoginActivity.kt
@@ -3,9 +3,11 @@ package com.example.tascs
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.tascs.databinding.ActivityLoginBinding
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.view.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -16,11 +18,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityLoginBinding= ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(activityLoginBinding.root)
+        val loginview: View =activityLoginBinding.root
+        setContentView(loginview)
+        loginview.signuplink.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+            finish()
+        }
 
-//        signuplink.setOnClickListener {
-//            startActivity(Intent(this,SignUpActivity::class.java))
-//                finish()
-//        }
     }
 }
