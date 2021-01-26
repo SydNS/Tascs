41c5c2631f045544fdba2bf9ec285f34f2090a35
SydNS <sydneynsiimbe@gmail.com>
2021-01-25 21:28:00 -0800

    added activityLoginBinding instance thats instantiated with the ActivityLoginBinding.inflate(layoutInflater) of which this layoutInflater is the layout for this activity
    then accessed the property of this instance for its root

--- app/src/main/java/com/example/tascs/LoginActivity.kt
+++ app/src/main/java/com/example/tascs/LoginActivity.kt
@@ -4,20 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.tascs.databinding.ActivityLoginBinding
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var activityLoginBinding:ActivityLoginBinding
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        signuplink.setOnClickListener {
-            startActivity(Intent(this,SignUpActivity::class.java))
-                finish()
-//            Toast.makeText(this,"deands",Toast.LENGTH_LONG)
-//                .show()
-        }
+        activityLoginBinding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(activityLoginBinding.root)
+
+//        signuplink.setOnClickListener {
+//            startActivity(Intent(this,SignUpActivity::class.java))
+//                finish()
+//        }
     }
 }
